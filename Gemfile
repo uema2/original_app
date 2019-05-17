4cf61ceffe4ee9eb033cf57b53259d5e92ec6efc
EC2 Default User <ec2-user@ip-172-31-94-31.ec2.internal>
2019-05-17 14:52:02 +0000

    for heroku

--- Gemfile
+++ Gemfile
@@ -34,4 +34,9 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :production do
+  gem 'pg', '0.21.0'
+  gem 'fog', '1.42'
+end
+
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -2,8 +2,11 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [400, 267]
 
-  storage :file
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
